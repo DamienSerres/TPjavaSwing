50070b3dd065cf5c1a888aff8b388d0dc1d3ccc2
damieninstitutd <damien@institut.design>
2020-10-29 17:10:04 +0100

    Correction Cartes

--- src/cartes/Cartes.java
+++ src/cartes/Cartes.java
@@ -5,10 +5,49 @@
  */
 package cartes;
 
+import java.util.Arrays;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Damien
  */
 public class Cartes {
-    
+
+    final static String[] valeurs
+            = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Valet",
+                "Dame", "Roi"};
+    final static String[] couleurs
+            = {"Pique", "Coeur", "Trèfle", "Carreau"};
+
+    private String valeur;
+    private String couleur;
+
+    public void setValeur(String valeur) {
+        if (Arrays.asList(this.valeurs).contains(valeur)) {
+            this.valeur = valeur;
+        } else {
+            System.out.println("Valeur impossible.");
+            JOptionPane.showMessageDialog(null, "Valeur impossible.");
+        }
+    }
+
+    public void setCouleur(String couleur) {
+        if (Arrays.asList(this.couleurs).contains(couleur)) {
+            this.couleur = couleur;
+        } else {
+            System.out.println("Couleur impossible.");
+            JOptionPane.showMessageDialog(null, "Couleur impossible.");
+        }
+    }
+
+    public static void main(String[] args) {
+        //JOptionPane.showMessageDialog(null, "Coucou");
+        Cartes test = new Cartes();
+        //test.setValeur("2");
+        //test.setValeur("68");
+        //test.setCouleur("Carreau");
+        test.setCouleur("Fleur des champs");
+
+    }
 }
